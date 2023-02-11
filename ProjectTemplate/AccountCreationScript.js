9e7ea922bf4eccfd7b6d087f0c3c90b0682c9350
Trevor <tdbontrager16@gmail.com>
2023-02-10 20:47:10 -0500

    added js to home and login

--- ProjectTemplate/AccountCreationScript.js
+++
@@ -1,1 +0,0 @@
-﻿// this will contain the code that links the backend account creation code with the front end page
--- ProjectTemplate/HomePageScript.js
+++
@@ -1,3 +0,0 @@
-﻿// this will be for the code that allows the home page to perform all the functions needed of it
-
-window.onload(alert("This is a test"));
